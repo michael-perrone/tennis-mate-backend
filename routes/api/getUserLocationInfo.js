273b3ada0c306652bff5d38aaa1a7790eb27fdd3
unknown <mikesdatascience@yahoo.com>
2019-10-03 02:47:49 -0400

    location working well for users

--- routes/api/getUserLocationInfo.js
+++ routes/api/getUserLocationInfo.js
@@ -6,7 +6,7 @@ router.post('/', async(req,res) => {
     let user = await User.findById({_id: req.body.userId})
     if (user) {
         try {
-            return res.status(200).json({userLocationSaved: user.locationSaved, userState: user.locationState, userTown: user.locationTown})
+            return res.status(200).json({userLocationSaved: user.locationSaved, locationDenied: user.locationDenied, userState: user.locationState, userTown: user.locationTown})
         }
         catch(error) {
         }
