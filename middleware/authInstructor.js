27a31654ce1a02c90a17a028b2986504ca51e573
Mike Perrone <mikeperrone@neverquitfoundation.org>
2019-10-29 22:11:03 -0400

    updated

--- middleware/authInstructor.js
+++ middleware/authInstructor.js
@@ -7,14 +7,19 @@ module.exports = function(req, res, next) {
   if (!token) {
     return res.status(401).json({ msg: "Not authorized" });
   }
+  console.log(token)
 
   try {
     const decodedToken = jwt.verify(token, config.get("instructorSecret"));
     console.log(decodedToken, "am i null");
     req.instructor = decodedToken.instructor;
+    console.log(decodedToken.instructor, "Im working")
     req.clubName = decodedToken.clubName;
     next();
   } catch (error) {
+    const decodedToken = jwt.verify(token, config.get("instructorSecret"));
+    console.log(decodedToken)
+    console.log()
     res.status(401).json({ msg: "Also not authorized" });
   }
 };
